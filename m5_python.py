321eeaffc5109eb0e61db49f9337f9da8afd0532
wunted <wunted@yandex.ru>
2016-12-17 14:26:41 +0300

    Bug fixed

--- m5_python.py
+++ m5_python.py
@@ -1,14 +1,14 @@
 def plus(list0):
     s = 0
-    for x in list0:
-        s += x
+    for i in list0:
+        s += i
     return s
 
 
 def plus1(list0):
     s = 0
-    a = len(list0)
-    while a > 0:
-        s += list0[a-1]
-        a -= 1
+    i = len(list0)
+    while i > 0:
+        s += list0[i-1]
+        i -= 1
     return s
