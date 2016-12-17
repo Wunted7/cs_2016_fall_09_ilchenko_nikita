51b7061533c409dac3b7681955fd7cafda0fd027
wunted <wunted@yandex.ru>
2016-12-17 16:24:23 +0300

    Third paragraph of first problem

--- m5_python.py
+++ m5_python.py
@@ -9,6 +9,16 @@ def plus1(list0):
     s = 0
     i = len(list0)
     while i > 0:
-        s += list0[i-1]
+        s += list0[i - 1]
         i -= 1
+    return s
+
+
+def plus2(list0):
+    i = len(list0)
+    s = list0[0]
+    if i == 1:
+        s = list0[0]
+    else:
+        s += plus2(list0[1:])
     return s
