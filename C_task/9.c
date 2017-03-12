e3efd127137cb6c7f85dc3981facc2dba3a167fa
Wunted7 <Wunted@yandex.ru>
2017-03-12 23:15:23 +0300

    Update 9.c

--- C_task/9.c
+++ C_task/9.c
@@ -2,28 +2,25 @@
 
 int fib(int n)
 {
-    int i = 0;
-    int c = 0;
-    int a = 0;
-    int b = 1;
     if (n == 0){
-        return printf("|%d|%d|\n",n,c); ;
+        return 0;
     }
-    for (i=0;i<=n;++i){
-        c=a;
-        a=b;
-        b=c+a;
+    else if(n == 1){
+        return 1;
     }
-    printf("|%d|%d|\n",n,c);
-    return fib(n-1);
+    else {
+        return fib(n-1)+fib(n-2);
+    }
 }
 
 int main()
 {
     int n = 0;
+    int i = 0;
     printf("Введите N\n");
     scanf("%i", &n);
-    fib(n);
-    printf("\n");
+    for (i=0;i<=n;++i){
+        printf("|%i|%i|\n", i, fib(i));
+    }
     return 0;
 }
