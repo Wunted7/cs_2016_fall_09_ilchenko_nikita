9b37c4a9f4106207249c4de6796de13f39f50f6b
Wunted7 <Wunted@yandex.ru>
2017-03-18 22:48:16 +0300

    Create 7.c

---
+++ C_task/7.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+   int x,y;
+   char z;
+   int i=0;
+   printf("Введите операцию:\n");
+   scanf("%i%c%i", &x,&z,&y);
+   int c=1;
+   switch(z){
+        case '+':
+            printf("%i\n", x+y);
+            break;
+        case '-':
+            printf("%i\n", x-y);
+            break;
+        case '*':
+            printf("%i\n", x*y);
+            break;
+        case '%':
+            printf("%i\n", x%y);
+            break;
+        case '^':
+            for(i=0;i<y;++i)
+                c*=x;
+            printf("%i\n", c);
+            break;
+   }
+    return 0;
+}
