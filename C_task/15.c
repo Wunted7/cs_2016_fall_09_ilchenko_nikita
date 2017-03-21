c41e0be9f1031d5ff91a907ff662852f9df12a56
Wunted7 <Wunted@yandex.ru>
2017-03-21 23:48:47 +0300

    Create 15.c

---
+++ C_task/15.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int i=0;
+    char a[1000], b[1000];
+    printf("Введите строку:\n");
+    scanf("%s", a);
+    for (i=0; i<strlen(a);++i){
+        
+    }
+    return 0;
+}
