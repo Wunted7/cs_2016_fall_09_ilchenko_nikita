c043789cc97bacd6e9c512acdc084a9d336bbd90
Wunted7 <Wunted@yandex.ru>
2017-03-23 21:32:21 +0300

    Update 15.c

--- C_task/15.c
+++ C_task/15.c
@@ -3,12 +3,20 @@
 
 int main()
 {
-    int i=0;
-    char a[1000], b[1000];
+    int j=0, i=0, l=0;
+    char a[1000], b[1000], c;
     printf("Введите строку:\n");
-    scanf("%s", a);
-    for (i=0; i<strlen(a);++i){
-        
+    while((c=getchar())!='\n'){
+        a[i]=c;
+        ++l;
+        ++i;
     }
+    for (i=1; i<=l;++i){
+        if(a[i-1]!=a[i]){
+            b[j]=a[i-1];
+            ++j;
+        }
+    }
+    printf("%s\n", b);
     return 0;
 }
