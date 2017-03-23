f01c7aef8b5b649bceb4096c4e0cb8696f3c831c
Wunted7 <Wunted@yandex.ru>
2017-03-23 20:51:09 +0300

    Update 6.c

--- C_task/6.c
+++ C_task/6.c
@@ -4,17 +4,17 @@ int main()
 {
     int x = 0;
     int i = 0;
-    int f = 1;
+    unsigned long int f = 1;
     printf("Введите число\n");
-    scanf("%i", &x);
+    scanf("%d", &x);
     if ((x==0) || (x==1)){
-        printf("Факториал = %i\n", f);
+        printf("Факториал = %d\n", f);
     }
     else{
         for (i=1;i<=x;++i){
             f*=i;
         }
-        printf("Факториал = %i\n", f);   
+        printf("Факториал = %d\n", f);   
     }
     
     return 0;
