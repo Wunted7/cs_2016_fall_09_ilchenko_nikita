2b2deb2ae2bbc5980190b5103c25a5bf177d89fb
Wunted7 <Wunted@yandex.ru>
2017-03-26 17:35:46 +0300

    Update 2.c

--- C_task/2.c
+++ C_task/2.c
@@ -4,10 +4,10 @@ int main()
 {
 	int day=17;
 	int month=9;
-	printf("%i.%i", day, month);
+	printf("%i.%i\n", day, month);
 	int c=day;
 	day=month;
 	month=c;
-	printf("%i.%i", day, month);
+	printf("%i.%i\n", day, month);
 	return 0;
 }
