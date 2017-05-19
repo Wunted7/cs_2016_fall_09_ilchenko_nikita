b82aa013870694166d3406cdb67298600fd78d0b
Wunted7 <Wunted@yandex.ru>
2017-05-20 00:02:49 +0300

    Create Derivative.c

---
+++ Modul_5/Derivative.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+double function(double x) {
+	double func;
+	func = x*x + x;
+	return func;
+}
+
+int main()
+{
+
+	double h, x, a, b;
+	double Dif = 0;
+	double D[] = { 0.0, 0.0 };
+
+	char sign;
+	scanf("%c", &sign);
+	char string[] = "%.xf\n";
+	string[2] = sign;
+
+	printf("Enter low and high limits: ");
+	scanf("%lf %lf", &a, &b);
+	printf("Your step: ");
+	scanf("%lf", &h);
+	x = a;
+	D[0] = (1 / (2 * h))*((-3) * function(x) + 4 * (function(x + h)) - (function(x + 2 * h)));
+	D[1] = (1 / (2 * h))*(-function(x) + function(x + 2 * h));
+	printf(string, D[0]);
+	printf(string, D[1]);
+	while (x + 2 * h <= b) {
+		Dif = (1 / (2 * h))*(function(x) - 4 * (function(x + h)) + 3 * (function(x + 2 * h)));
+		x = x + h;
+		printf(string, Dif);
+	}
+	return 0;
+}
